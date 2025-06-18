1453e420157d2fdb2b503cb33dfb49c4824417c3
pakjemetbleek2 <pakjemetbleek2@gmail.com>
2025-06-18 11:27:21 +0200

    Add files via upload
    dog

---
+++ dogvirus troll.ps1
@@ -0,0 +1,9 @@
+#Doel : Open een willekeurige foto van een hond
+#-lt geeft aan hoe vaak de code moet worden uitgevoerd, dus bij 3 laat hij 3 fotos zien
+for ($teller = 0; $teller -lt 3; $teller++) {
+    Write-Host $teller
+    $data = Invoke-WebRequest -Uri "https://dog.ceo/api/breeds/image/random"
+    $json = $data.content | ConvertFrom-Json
+    $url = $json.message
+    Start-Process "msedge.exe" -ArgumentList "--new-window", "$url"
+}
